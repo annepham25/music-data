7471fcedc23be2d5a753c71454385cc81f11cfaf
michaelz <michaelz@uw.edu>
2019-06-04 19:28:09 -0700

    updated the plot

--- music_app/server.R
+++ music_app/server.R
@@ -10,19 +10,22 @@
 library(shiny)
 library(dplyr)
 library(DT)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  raw_dataset <- read.csv("data/featuresdf.csv", stringsAsFactors = FALSE)
   
-  output$plot <- renderPlot({})
+  output$plot <- renderPlot({
+    ggplot(raw_dataset, aes_string(x = input$select1 , y = input$select2)) +
+      geom_point() +
+      geom_smooth(method = lm)
+  })
+  
   output$table <-  DT::renderDataTable({
     table <- raw_dataset %>%  select(artists, name, danceability) %>% 
       arrange(desc(danceability))  %>% top_n(input$slider)
     datatable(table, rownames = FALSE)
-    
   })
-   
-  
-  
 })
