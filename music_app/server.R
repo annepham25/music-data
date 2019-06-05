77761298595fe664a3183e0c88258b5dc5f0d5dc
annep7-1760336 <puppy2584@yahoo.com>
2019-06-04 19:44:09 -0700

    working on bar graph

--- music_app/server.R
+++ music_app/server.R
@@ -10,12 +10,34 @@
 library(shiny)
 library(dplyr)
 library(DT)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  # to read the music data
+  readData <- reactive({
+    data <- read.csv("data/featuresdf.csv")
+    data
+  })
   
-  output$plot <- renderPlot({})
+  output$plot <- renderPlot({
+    music_data <- readData()
+    
+    select <- input$select
+    select2 <- input$select2
+
+    counts <- as.data.frame(table(music_data[select]))
+    names(counts)[1] = "Type"
+
+    counts2 <- as.data.frame(table(music_data[select2]))
+    names(counts)[1] = "Type2"
+
+    ggplot(counts, aes(x = Type, y = Type2)) +
+      geom_bar(stat = "identity") +
+      labs(title = paste0("Number of reported UFOs sorted by ", input$select, " in the ", input$select2),x = select ,y = select2)
+  })
+  
   output$table <-  DT::renderDataTable({
     table <- raw_dataset %>%  select(artists, name, danceability) %>% 
       arrange(desc(danceability))  %>% top_n(input$slider)
